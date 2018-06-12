b6833601f0716f50fd26c1ceb347ccaebb09b289
Electrux Redsworth <ElectruxRedsworth@gmail.com>
2018-06-13 01:32:25 +0530

    Updated to v0.7.2

--- Formula/ccp4m.rb
+++ Formula/ccp4m.rb
@@ -1,8 +1,8 @@
 class Ccp4m < Formula
   desc "C++ project manager and build system"
   homepage "https://github.com/Electrux/ccp4m"
-  url "https://github.com/Electrux/ccp4m/archive/v0.7.1.tar.gz"
-  sha256 "f6db766bf045023c9387ffb0a7c2c8fa78b012783e244b1c98126c2cb5ca0072"
+  url "https://github.com/Electrux/ccp4m/archive/v0.7.2.tar.gz"
+  sha256 "83df16dd691fa1e111eb4b094ffc8b8da6653716f76c5c90ab13d1ac7e626577"
 
   depends_on "yaml-cpp" => :build
 
